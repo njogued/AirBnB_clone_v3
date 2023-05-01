894099ae2378c4f7e09c132198e0177c52561ab5
njogued <njogued@gmail.com>
2023-05-01 03:30:42 -0700

    Updated the stats path

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -17,7 +18,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def display_stats():
     classes = {"amenities": Amenity, "cities": City,
                "places": Place, "reviews": Review,
