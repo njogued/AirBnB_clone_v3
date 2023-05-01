14f9ca2299f7d467f8ce1d88aede2ce7050884c1
njogued <njogued@gmail.com>
2023-05-01 02:26:26 -0700

    Put stats in correct file, removed unnecessary jsonfiy formatting

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,32 +4,12 @@
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, jsonify
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 from os import getenv
 
-"""comment this out to use jsonify instead"""
-import json
-from flask import make_response
-from json import dumps
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
-def jsonify(status=200, indent=4, sort_keys=True, **kwargs):
-    """Comment this part out to return unformatted json response"""
-    """Overwriting the jsonify function to return pretty output"""
-    response = make_response(dumps(dict(**kwargs),
-                                   indent=indent, sort_keys=sort_keys))
-    response.headers['Content-Type'] = 'application/json; charset=utf-8'
-    response.headers['mimetype'] = 'application/json'
-    response.status_code = status
-    return response
 
 @app.teardown_appcontext
 def tear_all(exception=None):
@@ -37,22 +17,6 @@ def tear_all(exception=None):
     Not specified in instruction but tear_all must have
     one argument, else TypeError"""
     storage.close()
-
-
-@app.route('/api/v1/stats', strict_slashes=False)
-def display_stats():
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    clss_stats = {}
-    for key, value in classes.items():
-        clss_stats[key] = storage.count(value)
-    return jsonify(**clss_stats)
-    """Should probably use jsonify instead of json.dumps but idk
-    #return jsonify(clss_stats)
-    #return str(storage.count(State))
-    Using json.dumps for more readable output
-    #return json.dumps(clss_stats, indent=4)"""
 
 
 @app.errorhandler(404)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,24 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON of status when queried"""
     return jsonify({"status": "OK"})
+
+@app.route('/api/v1/stats', strict_slashes=False)
+def display_stats():
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review,
+               "states": State, "users": User}
+    clss_stats = {}
+    for key, value in classes.items():
+        clss_stats[key] = storage.count(value)
+    return jsonify(clss_stats)
